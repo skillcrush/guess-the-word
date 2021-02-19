bf848b34c8b436f56ccea5b87137ee37ef008609
Ann Cascarano <a.cascarano@gmail.com>
2021-02-19 16:52:40 -0500

    Function expressions are nice

--- js/script.js
+++ js/script.js
@@ -1,120 +1,117 @@
-const guessedLettersElement = document.querySelector(".guessed-letters");
-const guessLetterButton = document.querySelector(".guess");
-const letterInput = document.querySelector(".letter");
-const word = "magnolia"; // substitute for a 'getWord' function if we decide to use a large array
-const wordInProgress = document.querySelector(".word-in-progress");
-let remainingGuesses = 6; // Let's output this to screen
-const remainingGuessesElement = document.querySelector(".remaining span");
-const guessedLetters = []; 
-const message = document.querySelector(".message");
+const guessedLettersElement = document.querySelector('.guessed-letters')
+const guessLetterButton = document.querySelector('.guess')
+const letterInput = document.querySelector('.letter')
+const word = 'magnolia' // substitute for a 'getWord' function if we decide to use a large array
+const wordInProgress = document.querySelector('.word-in-progress')
+let remainingGuesses = 6 // Let's output this to screen
+const remainingGuessesElement = document.querySelector('.remaining span')
+const guessedLetters = []
+const message = document.querySelector('.message')
 
 // Display our wordplaceholder at first paint
-const placeholder = function(){
-  const wordArray = word.toUpperCase().split("");
-  const placeholderLetters = [];
-  for(let letter of wordArray){
-    placeholderLetters.push("☀️")
+const placeholder = function () {
+  const wordArray = word.toUpperCase().split('')
+  const placeholderLetters = []
+  // eslint-disable-next-line no-unused-vars
+  for (const _letter of wordArray) {
+    placeholderLetters.push('☀️')
   }
-  wordInProgress.innerText = placeholderLetters.join("");
+  wordInProgress.innerText = placeholderLetters.join('')
 }
-placeholder();
+placeholder()
 
-guessLetterButton.addEventListener("click", function () {
+guessLetterButton.addEventListener('click', () => {
   // Empty message paragraph
-  message.innerText = "";
+  message.innerText = ''
 
   // Let's grab what was entered in the input
-  const guess = letterInput.value.toUpperCase();
+  const guess = letterInput.value.toUpperCase()
   // Let's make sure that it is a single letter
-  const goodGuess = validateInput(guess);
+  const goodGuess = validateInput(guess)
 
   if (goodGuess) {
     // We've got a letter, let's guess!
-    makeGuess(guess);
+    makeGuess(guess)
   }
-  
+
   // Show user what they already guessed
-  showGuessedLetters();
-});
+  showGuessedLetters()
+})
 
 const showGuessedLetters = function () {
-    guessedLettersElement.innerText = guessedLetters;
-};
+  guessedLettersElement.innerText = guessedLetters
+}
 
 const validateInput = function (input) {
-  const acceptedLetter = /[A-Z]/;
+  const acceptedLetter = /[A-Z]/
   if (input.length === 0) {
     // Is the input empty?
-    message.innerText = `Please enter a letter`;
-    return;
+    message.innerText = 'Please enter a letter'
   } else if (input.length > 1) {
     // Did you type more than one letter?
-    message.innerText = `Please enter a single letter`;
-    return;
+    message.innerText = 'Please enter a single letter'
   } else if (!input.match(acceptedLetter)) {
     // Did you type a number, a special character or some other non letter thing?
-    message.innerText = `We need a letter from A to Z, please.`;
-    return;
+    message.innerText = 'We need a letter from A to Z, please.'
   } else {
     // We finally got a single letter, omg yay
-    return input;
+    return input
   }
-};
+}
 
 const makeGuess = function (guess) {
   if (guessedLetters.includes(guess)) {
-    message.innerText = "You already guessed that letter, silly. Try again.";
+    message.innerText = 'You already guessed that letter, silly. Try again.'
   } else {
-    guessedLetters.push(guess);
-    updateGuessesRemaining(guess); // we may or may not want this here
+    guessedLetters.push(guess)
+    updateGuessesRemaining(guess) // we may or may not want this here
   }
   // New letter guessed - let's see if we're right
-  updateWord(guessedLetters);
-};
+  updateWord(guessedLetters)
+}
 
-function updateGuessesRemaining(guess){
-  const wordArray = word.toUpperCase().split("");
-  if (!wordArray.includes(guess)){
+const updateGuessesRemaining = function (guess) {
+  const wordArray = word.toUpperCase().split('')
+  if (!wordArray.includes(guess)) {
     // womp womp - bad guess, lose a chance
     message.innerText = `Sorry, the word has no ${guess}.`
-    remainingGuesses -= 1;
+    remainingGuesses -= 1
   } else {
     message.innerText = `Yep, we've got a ${guess} - good guess!`
   }
 
-  if (remainingGuesses === 0){
-    message.innerText = `GAME OVER. The word was ${word}`;
-    playAgain
+  if (remainingGuesses === 0) {
+    message.innerText = `GAME OVER. The word was ${word}`
+    playAgain()
   }
-  remainingGuessesElement.innerText = remainingGuesses;
+  remainingGuessesElement.innerText = remainingGuesses
 }
 
-const updateWord = function(guessedLetters){
-
-  const wordArray = word.toUpperCase().split("");
-  const revealWord = [];
-  for(let letter of wordArray){
-    if(guessedLetters.includes(letter)){
-      revealWord.push(letter.toUpperCase());
+const updateWord = function (guessedLetters) {
+  const wordArray = word.toUpperCase().split('')
+  const revealWord = []
+  for (const letter of wordArray) {
+    if (guessedLetters.includes(letter)) {
+      revealWord.push(letter.toUpperCase())
     } else {
-      revealWord.push("☀️")
+      revealWord.push('☀️')
     }
   }
   console.log(revealWord)
-  wordInProgress.innerText = revealWord.join("");
+  wordInProgress.innerText = revealWord.join('')
 
-  checkIfWin();
+  checkIfWin()
 }
 
-const checkIfWin = function(){
-  if (word.toUpperCase() === wordInProgress.innerText){
-    message.innerText = "You guessed the word!!!! WOOOO!!!"
-    playAgain();
+const checkIfWin = function () {
+  if (word.toUpperCase() === wordInProgress.innerText) {
+    message.innerText = 'You guessed the word!!!! WOOOO!!!'
+    playAgain()
   }
 }
 
-const playAgain = function(){
-  console.log("playAgain function fires now. Todo.")
+const playAgain = function () {
+  console.log('playAgain function fires now. Todo.')
   // shows playAgain button
   // resets number of chances
   // picks new word
