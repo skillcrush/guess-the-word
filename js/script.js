f5aa3914e5a7af278dc0683fc6601396efdbe840
Ann Cascarano <a.cascarano@gmail.com>
2021-03-08 12:59:34 -0500

    Simplify placeholder function and small fix

--- js/script.js
+++ js/script.js
@@ -28,13 +28,11 @@ const getWord = async function () {
 getWord();
 // Display our symbols as placeholders for the chosen word's letters
 const placeholder = function (word) {
-  console.log(word);
-  const wordArray = word.toUpperCase().split("");
-  console.log(wordArray);
   const placeholderLetters = [];
-  wordArray.forEach(function (letter) {
+  for (const letter of word) {
+    console.log(letter);
     placeholderLetters.push("☀️");
-  });
+  }
   wordInProgress.innerText = placeholderLetters.join("");
 };
 
@@ -43,7 +41,7 @@ guessLetterButton.addEventListener("click", function (e) {
   // Empty message paragraph
   message.innerText = "";
   // Let's grab what was entered in the input
-  const guess = letterInput.value.toUpperCase();
+  const guess = letterInput.value;
   // Let's make sure that it is a single letter
   const goodGuess = validateInput(guess);
 
@@ -55,7 +53,7 @@ guessLetterButton.addEventListener("click", function (e) {
 });
 
 const validateInput = function (input) {
-  const acceptedLetter = /[A-Z]/;
+  const acceptedLetter = /[a-zA-Z]/;
   if (input.length === 0) {
     // Is the input empty?
     message.innerText = "Please enter a letter";
@@ -71,6 +69,19 @@ const validateInput = function (input) {
   }
 };
 
+const makeGuess = function (guess) {
+  guess = guess.toUpperCase();
+  if (guessedLetters.includes(guess)) {
+    message.innerText = "You already guessed that letter, silly. Try again.";
+  } else {
+    guessedLetters.push(guess);
+    console.log(guessedLetters);
+    updateGuessesRemaining(guess);
+    showGuessedLetters();
+    updateWordInProgress(guessedLetters);
+  }
+};
+
 const showGuessedLetters = function () {
   // Clear the list first
   guessedLettersElement.innerHTML = "";
@@ -78,18 +89,6 @@ const showGuessedLetters = function () {
     const li = document.createElement("li");
     li.innerText = letter;
     guessedLettersElement.append(li);
-  }
-};
-
-const makeGuess = function (guess) {
-  if (guessedLetters.includes(guess)) {
-    message.innerText = "You already guessed that letter, silly. Try again.";
-  } else {
-    guessedLetters.push(guess);
-    console.log(guessedLetters);
-    updateGuessesRemaining(guess);
-    showGuessesdLetters();
-    updateWordInProgress(guessedLetters);
   }
 };
 
@@ -131,6 +130,6 @@ const updateGuessesRemaining = function (guess) {
 const checkIfWin = function () {
   if (word.toUpperCase() === wordInProgress.innerText) {
     message.classList.add("win");
-    message.innerHTML = "<p class=\"highlight\">You guessed the word!!!! WOOOO!!!</p>";
+    message.innerHTML = `<p class="highlight">You guessed the word!!!! WOOOO!!!</p>`;
   }
 };
