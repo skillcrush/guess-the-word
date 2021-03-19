51a2f61986f680839849be7f96043f010494ccd9
Nichole M <nichole@skillcrush.com>
2021-03-19 14:43:09 -0400

    Updated placeholder emoji

--- js/script.js
+++ js/script.js
@@ -14,7 +14,7 @@ const placeholder = function (word) {
   const placeholderLetters = [];
   for (const letter of word) {
     console.log(letter);
-    placeholderLetters.push("☀️");
+    placeholderLetters.push("●");
   }
   wordInProgress.innerText = placeholderLetters.join("");
 };
