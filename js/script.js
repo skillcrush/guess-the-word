6caf3e780fc3a1b17a4495c2a80060f863fe981c
Kesh-b <83928595+Kesh-b@users.noreply.github.com>
2023-01-24 21:22:59 -0700

    Update script.js
    part 3

--- js/script.js
+++ js/script.js
@@ -8,6 +8,8 @@ const message= document.querySelector(".message");
 const playAgainButton = document.querySelector(".play-again");
 const word = "magnolia";
 
+const usedLetters = [];
+
 
 const lettersUnknown = function (word) {
     const lettersNotKnown = [];
@@ -23,8 +25,40 @@ lettersUnknown(word);
 
 button.addEventListener("click", function (e) {
     e.preventDefault();
+    message.innerText = "";
+
     const letterGuessed = inputLetterGuess.value; 
-    console.log(letterGuessed);
+    const correctGuess = validate(letterGuessed); 
+
+    if (correctGuess){
+        makeGuess(letterGuessed);
+    }
+
     inputLetterGuess.value = "";
 }); 
 
+const validate = function (input) {
+    const acceptedLetter = /[a-zA-Z]/
+    
+    if (input.length === 0 ) {
+        message.innerText = "pick a letter please"; 
+    } else if ( input.length > 1) {
+        message.innerText = "only one letter silly";
+    } else if (input.match(acceptedLetter)) {
+        message.innerText = "has to be an actual letter";
+    } else {
+        return input; 
+    }
+}; 
+
+const makeGuess = function (letterGuessed) {
+  letterGuessed = letterGuessed.toUpperCase(); 
+    if (usedLetters.includes(letterGuessed)) {
+        message.innerText = "used that letter already";
+    } else {
+            letterGuessed.push(usedLetters);
+            console.log(usedLetters);
+    }
+
+  
+};
